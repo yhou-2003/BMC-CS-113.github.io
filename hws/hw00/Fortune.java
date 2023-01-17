c4e37c23602552c9f81430f940d07140f9934120
Adam Poliak <azpoliak@gmail.com>
2023-01-16 22:14:31 -0500

    HW00 released

---
+++ hws/hw00/Fortune.java
@@ -0,0 +1,10 @@
+// Name:
+// Date:
+// Description: Print a fortune to the console
+
+public class Fortune {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!"); // todo: your message here
+  }
+}
